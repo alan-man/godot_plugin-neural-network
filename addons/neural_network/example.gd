3909501325b27ede159efebd985c11271814392a
alan-man <alan.tambellinijonic@gmail.com>
2024-01-02 21:25:24 -0800

    xor test added to example.gd

--- addons/neural_network/example.gd
+++ addons/neural_network/example.gd
@@ -18,6 +18,40 @@ func basic_test() -> void:
 	test.show_result()
 	test.train(500)
 	test.show_result()
+	
+
+
+func xor_test() -> void:
+	var xor = NNET.new([2,5,5,1],0.1,true)
+	
+	xor.set_input([1,0])
+	xor.set_desired_output([1.0])
+	xor.run()
+	xor.show_result()
+	xor.train(500)
+	xor.show_result()
+	
+	xor.set_input([0,1])
+	xor.set_desired_output([1.0])
+	xor.run()
+	xor.show_result()
+	xor.train(500)
+	xor.show_result()
+	
+	xor.set_input([0,0])
+	xor.set_desired_output([0.0])
+	xor.run()
+	xor.show_result()
+	xor.train(500)
+	xor.show_result()
+	
+	xor.set_input([1,1])
+	xor.set_desired_output([0.0])
+	xor.run()
+	xor.show_result()
+	xor.train(500)
+	xor.show_result()
+
 
 func _ready() -> void:
 	print("Here, the neural network tries to make the output equal to 0.5. The training method used is back propagation.")
